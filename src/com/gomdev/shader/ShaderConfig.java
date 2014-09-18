a876c8f72ea866d596ab07feaf34b2d1be906847
gomdev <zergzerg@gmail.com>
2014-09-18 18:07:34 +0900

    remove unused constant string

--- src/com/gomdev/shader/ShaderConfig.java
+++ src/com/gomdev/shader/ShaderConfig.java
@@ -4,16 +4,6 @@ public class ShaderConfig {
     static final boolean DEBUG = false;
     static final String TAG = "gomdev";
 
-    static final String EXTRA_SHADER = "shader";
-
-    public static final String PREF_NAME = "effect_pref";
-    public static final String PREF_EFFECT_NAME = "effect_name";
-    public static final String PREF_SHADER_COUNT = "shader_count";
-    public static final String PREF_SHADER_TITLE = "shader_title";
-    public static final String PREF_SHADER_RES_ID = "res_id";
-    public static final String PREF_SAVED_FILE_NAME = "saved_file_name";
-    public static final String PREF_SAVED_RES_ID = "saved_res_id";
-
     public static final String APP_DIRECTORY_NAME = "gomdev";
     
     public static final boolean ENABLE_AD = true; 
